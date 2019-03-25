9934df98e8d6dbe640abd7ed51cde5bb992c6021
Aiilu <40794899+Aiilu@users.noreply.github.com>
2019-03-25 17:00:59 -0300

    Add files via upload

---
+++ Ansi_C/Ejercicios/Ejerc55/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+/*Un profesor de matemática de un establecimiento educativo registra de cada alumno N° de legajo y
+promedio. Según el promedio desea saber cuántos alumnos aprobaron (promedio mayor o igual a 7), cuantos rinden
+ en diciembre (promedio menor a 7 y mayor o igual a 4) y cuantos rinden examen en marzo
+(promedio menor a 4). Además desea conocer el N° de legajo del alumno con mejor promedio
