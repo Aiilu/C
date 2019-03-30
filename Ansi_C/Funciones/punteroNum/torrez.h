59afc1248407d2afee1b46711ffd18c025316b6f
Aiilu <40794899+Aiilu@users.noreply.github.com>
2019-03-29 22:14:31 -0300

    Add files via upload

---
+++ Ansi_C/Funciones/punteroNum/torrez.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float ingresarNumero();
+float sumar(float, float);
+float restar(float,float);
+float multiplicar(float,float);
+float dividir(float,float);
+float factorial(float,float);
+int getInt(int* y, int intentos, char* mes);
